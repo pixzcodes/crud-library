0cb189b459db9ed05af64ebf5b45ae2b570ffcc6
pixz <tinycactusking@gmail.com>
2025-01-23 13:12:06 -0500

    Refactored to use classes instead of objects

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // main array where the book objects will be stored
-const library = [];
+const libraryList = [];
 
 // Book constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    // this was more for testing purposes but
+    // I kept it for future use
+    info = function() {
         let readString;
         if (read) {
             readString = "has been read."
@@ -17,66 +22,70 @@ function Book(title, author, pages, read) {
         return this.title + " by " + this.author + ", " + this.pages + " pages, " + readString;
     }
 }
+class Library {
+    constructor() { }
 
-function addBookToLibrary(title, author, pages, read) {
-    // create the book
-    const newBook = new Book(title, author, pages, read);
+    static addBookToLibrary(title, author, pages, read) {
+        // create the book
+        const newBook = new Book(title, author, pages, read);
 
-    // add the book to the library array
-    library.push(newBook);
-}
-
-function displayAllBooks() {
-    // get book shelves element
-    const shelves = document.getElementById("book-shelves");
-
-    // first clear the shelves
-    shelves.innerHTML = "";
-
-    for (let i = 0; i < library.length; i++) {
-        // book object we are using
-        const book = library[i];
-        // create a div element for the book and append the
-        // book info text to the element
-        const newBook = document.createElement("div");
-
-        // create button for removing book
-        const removeBtn = document.createElement("button");
-        removeBtn.innerHTML = "Remove";
-        removeBtn.classList.add("remove");
-        removeBtn.addEventListener("click", (e) => {
-            e.preventDefault();
-            library.splice(i, 1);
-            displayAllBooks();
-        });
-
-        // create change read button
-        const toggleReadBtn = document.createElement("button");
-        toggleReadBtn.innerHTML = "Toggle Read";
-        toggleReadBtn.classList.add("toggleRead");
-        toggleReadBtn.addEventListener("click", (e) => {
-            e.preventDefault();
-            if (book.read) {
-                book.read = false;
-            }
-            else {
-                book.read = true;
-            }
-            displayAllBooks();
-        });
-
-        // set the class
-        newBook.classList.add("book");
-        //  and id for the book based on position in library array
-        newBook.setAttribute("id", i);
-
-
-        newBook.innerHTML = `<h4>${book.title}</h4><p>by ${book.author}</p><p>pages: ${book.pages}</p><p>read: ${book.read}</p>`;
-        newBook.appendChild(removeBtn);
-        newBook.appendChild(toggleReadBtn);
-
-        shelves.appendChild(newBook);
+        // add the book to the library array
+        libraryList.push(newBook);
     }
+
+    static displayAllBooks() {
+        // get book shelves element
+        const shelves = document.getElementById("book-shelves");
+
+        // first clear the shelves
+        shelves.innerHTML = "";
+
+        for (let i = 0; i < libraryList.length; i++) {
+            // book object we are using
+            const book = libraryList[i];
+            // create a div element for the book and append the
+            // book info text to the element
+            const newBook = document.createElement("div");
+
+            // create button for removing book
+            const removeBtn = document.createElement("button");
+            removeBtn.innerHTML = "Remove";
+            removeBtn.classList.add("remove");
+            removeBtn.addEventListener("click", (e) => {
+                e.preventDefault();
+                libraryList.splice(i, 1);
+                Library.displayAllBooks();
+            });
+
+            // create change read button
+            const toggleReadBtn = document.createElement("button");
+            toggleReadBtn.innerHTML = "Toggle Read";
+            toggleReadBtn.classList.add("toggleRead");
+            toggleReadBtn.addEventListener("click", (e) => {
+                e.preventDefault();
+                if (book.read) {
+                    book.read = false;
+                }
+                else {
+                    book.read = true;
+                }
+                Library.displayAllBooks();
+            });
+
+            // set the class
+            newBook.classList.add("book");
+            //  and id for the book based on position in library array
+            newBook.setAttribute("id", i);
+
+
+            newBook.innerHTML = `<h4>${book.title}</h4><p>by ${book.author}</p><p>pages: ${book.pages}</p><p>read: ${book.read}</p>`;
+            newBook.appendChild(removeBtn);
+            newBook.appendChild(toggleReadBtn);
+
+            shelves.appendChild(newBook);
+        }
+    }
+
 }
 
 // adding click event for new book button
@@ -87,7 +96,6 @@ newBookBtn.addEventListener("click", () => {
     // hide the new book button too
     newBookBtn.toggleAttribute("hidden");
 });
-
 const submitBtn = document.querySelector("#btnSubmit");
 submitBtn.addEventListener("click", (e) => {
     // first prevent the button doing what it's supposed to
@@ -109,8 +117,8 @@ submitBtn.addEventListener("click", (e) => {
     }
 
     // add and display the book
-    addBookToLibrary(title.value, author.value, pages.value, read);
-    displayAllBooks();
+    Library.addBookToLibrary(title.value, author.value, pages.value, read);
+    Library.displayAllBooks();
 
     // re-hide the form and un-hide the new book button
     const newBookBtn = document.querySelector("#btnNewBook");
@@ -120,8 +128,9 @@ submitBtn.addEventListener("click", (e) => {
 
 });
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
-addBookToLibrary("Harry Potter", "J.K. Rowling", 400, false);
-displayAllBooks();
+
+Library.addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+Library.addBookToLibrary("Harry Potter", "J.K. Rowling", 400, false);
+Library.displayAllBooks();
 
 
